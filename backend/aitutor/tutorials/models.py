643183466d69a71ae68cd25a980d5ed40763367e
Bogdan Muhanov <bogdanmuhanov95@gmail.com>
2024-11-18 21:25:22 +0300

    Add new models and views I forgot about a wonderful feature django has(forgot about switching branches too). Replace sql scripts with models. Also add API endpoint to tutorials app, serializers and viewset for Language model

--- backend/aitutor/tutorials/models.py
+++ backend/aitutor/tutorials/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Manual(models.Model):
+    name = models.CharField(max_length=255)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    manual = models.ForeignKey(Manual, on_delete=models.CASCADE)
+
+
+class Tutorial(models.Model):
+    name = models.CharField(max_length=255)
+    content = models.TextField()
+    publication_date = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
