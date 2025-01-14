e4bb8a2ce2c4e76eae148f94ad6a9d55a749c7fd
Muhanov Bogdan <bogdanmuhanov95@gmail.com>
2025-01-15 00:05:27 +0300

    Add tutorials and home page

--- database/queries/select_all_tables_with_join.sql
+++
@@ -1,5 +0,0 @@
-select l_t.name, m_t.name, t_t.name, tu_t.name
-from languages l_t
-join manuales m_t ON m_t.language_id = l_t.id
-join topics t_t ON t_t.manuale_id = m_t.id
-join tutorials tu_t ON tu_t.topic_id = t_t.id
