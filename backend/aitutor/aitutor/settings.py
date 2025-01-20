edb19672dcb85b8d4f1b95ee553e25a9c8cdcd47
Muhanov Bogdan <bogdanmuhanov95@gmail.com>
2025-01-20 16:53:25 +0300

    improve courses for kotlin and python

--- backend/aitutor/aitutor/settings.py
+++ backend/aitutor/aitutor/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "NAME": db_name,
         "USER": db_user,
         "PASSWORD": db_pass,
-        "HOST": 'db',
+        "HOST": 'localhost',
         "PORT": '5432',
     }
 }
